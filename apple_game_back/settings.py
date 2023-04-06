c85c4c1a0088ef65f8d1c33c94475eb2af5f6ee7
dev-dong-su <fkdlrj1230@gmail.com>
2023-04-06 15:04:58 +0900

    chore: 빌드 오류 수정

--- apple_game_back/settings.py
+++ apple_game_back/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'apple_game_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'apple_game',
-        'USER': 'devdongsu',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
