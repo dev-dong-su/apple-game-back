f31d5a04b22e434071e727f567c47455b1e50085
dev-dong-su <fkdlrj1230@gmail.com>
2023-04-11 10:02:19 +0900

    feat: admin game session 필터 기능

--- apple_game/admin.py
+++ apple_game/admin.py
@@ -8,8 +8,9 @@ class SaladLabAdmin(admin.ModelAdmin):
 
 
 class GameSessionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'score', 'is_completed')
-    ordering = ('user',)
+    list_display = ('user', 'score', 'is_completed', 'start_time')
+    ordering = ('-start_time',)  # start_time을 기준으로 내림차순 정렬
+    list_filter = ('is_completed',)  # is_completed 필드를 기준으로 필터링 옵션 추가
 
 
 admin.site.register(SaladLab, SaladLabAdmin)
